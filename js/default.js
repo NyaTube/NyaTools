2bfe7cd0d9318250350f49edb6ddba2142ec6e97
NyaTube <yukikun.neko@gmail.com>
2022-03-13 00:12:28 +0900

    jsの変数宣言をvarからlet変更

--- js/default.js
+++ js/default.js
@@ -1,18 +1,18 @@
 function calculation() {
 
-    var work_time = $('select[name="work_time"]').val();
-    var work_date = $('input[name="work_date"]').val();
-    var work_time_min = $('select[name="work_time_min"]').val();
+    let work_time = $('select[name="work_time"]').val();
+    let work_date = $('input[name="work_date"]').val();
+    let work_time_min = $('select[name="work_time_min"]').val();
     console.log("work_time_min" + work_time_min);
     console.log("work_time" + work_time);
     console.log("work_date" + work_date);
 
-    var calculation_result = work_time * work_date;
+    let calculation_result = work_time * work_date;
     console.log("calculation_result" + calculation_result);
-    var ExtraTime = calculation_result - work_time_min;
+    let ExtraTime = calculation_result - work_time_min;
     console.log("ExtraTime" + ExtraTime);
     console.log(Math.floor(ExtraTime/work_time)); 
-    var holidays = Math.floor(ExtraTime/work_time);
+    let holidays = Math.floor(ExtraTime/work_time);
     
     if (Math.sign(holidays) !== 1) {
         holidays = 0;
